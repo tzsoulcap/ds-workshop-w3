5aee17c04d90d67aea656a02e5355774daf5cd79
Chayodom Heha <tzsoulcap@gmail.com>
2023-10-21 12:00:54 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from prophet.serialize import model_to_json, model_from_json
+import plotly.express as px
+
+def load_data():
+    df = pd.read_csv('TSLA.csv')
+    columns = ['Date', 'Close']
+    ndf = pd.DataFrame(df, columns = columns)
+    # ndf = ndf.rename(columns = {'Date': 'ds', 'Close': 'y'})
+    return ndf
+
+def join_data(df1, df2):
+    df1 = df1.loc[df2.shape[0]:, ['ds', 'yhat']]  \
+           .rename(columns={'ds': 'Date', 'yhat': 'Close'})
+    df = pd.concat([df1, df2], axis=0).reset_index(drop=True)
+    df['Date'] = pd.to_datetime(df['Date'].astype(str), format='%Y-%m-%d')
+    df = df.sort_values(by='Date', ascending=False).reset_index(drop=True)
+    return df
+
+
+# st.session_state['age'] = 0
+with open('serialized_model.json', 'r') as fin:
+    m = model_from_json(fin.read())  # Load model
+
+st.title('Predictive Model')
+
+st.slider('Select periods (Day)', min_value=1, max_value=100, value=50, step=1, key='day')
+st.write(st.session_state['day'])
+
+future = m.make_future_dataframe(periods=st.session_state['day'], freq='D')
+forecast = m.predict(future)
+
+figure = m.plot(forecast, xlabel='ds', ylabel='yhat', include_legend=True)
+st.write(figure)
+
+predicted = join_data(forecast, load_data())
+st.write(px.line(predicted, x='Date', y='Close'))
+st.write(predicted)
